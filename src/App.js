e446c9eb0547c687b21201d881d791fb1032ccef
prashant_jawale <prashant.jawale@e-zest.in>
2022-06-14 17:24:22 +0530

    first commit of booling app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+
+function App() {
+  useEffect(() => {
+    alert("Hey Welcome !! This is demo project UI of Booking site using React");
+  }, []);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
